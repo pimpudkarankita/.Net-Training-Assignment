9086b8b1b3e5b5d6ae543b78c3b4c22fc91c84cf
pimpudkarankita <87276247+pimpudkarankita@users.noreply.github.com>
2021-07-14 22:51:58 +0530

    Add files via upload

---
+++ Break.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Break
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+    
+
