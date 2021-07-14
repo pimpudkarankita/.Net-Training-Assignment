1fc8e746bc505b9ec23b25edfb97c7e6829fe2d7
pimpudkarankita <87276247+pimpudkarankita@users.noreply.github.com>
2021-07-14 22:59:34 +0530

    Add files via upload

---
+++ console.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class console
+    
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Class1 c1 = new Class1();
+                int a = c1;
+                Console.WriteLine(a);
+                int b = c1;
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
+
