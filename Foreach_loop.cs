110a4fa7632d538a20b5aad554074278b30661dc
pimpudkarankita <87276247+pimpudkarankita@users.noreply.github.com>
2021-07-15 14:26:38 +0530

    Add files via upload

---
+++ Foreach_loop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Foreach_loop
+    {
+        static public void Main()
+        {
+
+            Console.WriteLine("Print array");
+
+       
+            int[] a_array = new int[] { 1, 2, 3, 4, 5 };
+
+          
+            foreach (int items in a_array)
+            {
+                Console.WriteLine(items);
+            }
+        }
+    }
+}
+    
+
